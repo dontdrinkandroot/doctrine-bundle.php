c7f98552d7ae9a2c2a9a5ed5b4777cd25cbd4c5e
Philip Washington Sorst <philip@sorst.net>
2016-02-29 21:31:11 +0100

    Creating Symfony Structure

---
+++ DdrDoctrineBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dontdrinkandroot\DoctrineBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DdrDoctrineBundle extends Bundle
+{
+}
---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dontdrinkandroot\DoctrineBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ddr_doctrine');
+
+        return $treeBuilder;
+    }
+}
