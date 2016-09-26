4b34286646b15c95c3877aaa767b6a8bc5974631
Philip Washington Sorst <philip@sorst.net>
2016-09-26 17:53:35 +0200

    Adding assertedReference

--- Tests/ReferenceTrait.php
+++ Tests/ReferenceTrait.php
@@ -4,6 +4,21 @@ namespace Dontdrinkandroot\DoctrineBundle\Tests;
 
 trait ReferenceTrait
 {
+    /**
+     * @param string $name
+     * @param string $class
+     *
+     * @return mixed
+     */
+    protected function getAssertedReference($name, $class)
+    {
+        $reference = $this->getReference($name);
+        if (!is_a($reference, $class)) {
+            throw new \RuntimeException(sprintf('"%s" is not a "%s"', $name, $class));
+        }
+
+        return $reference;
+    }
 
     /**
      * @param string $name
