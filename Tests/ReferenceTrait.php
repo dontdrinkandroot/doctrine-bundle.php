10a8719bed289b863a56f5502075f11256934f3f
Philip Washington Sorst <philip@sorst.net>
2016-09-30 17:14:10 +0200

    Removing getAssertedReference as it will cause conflicts

--- Tests/ReferenceTrait.php
+++ Tests/ReferenceTrait.php
@@ -6,22 +6,6 @@ trait ReferenceTrait
 {
     /**
      * @param string $name
-     * @param string $class
-     *
-     * @return mixed
-     */
-    protected function getAssertedReference($name, $class)
-    {
-        $reference = $this->getReference($name);
-        if (!is_a($reference, $class)) {
-            throw new \RuntimeException(sprintf('"%s" is not a "%s"', $name, $class));
-        }
-
-        return $reference;
-    }
-
-    /**
-     * @param string $name
      *
      * @return mixed
      */
