0f2c2f078b5419b94faa10b2a9effb2c99e1f2f7
Philip Washington Sorst <philip@sorst.net>
2019-07-14 22:21:57 +0200

    Moving Event listeners

--- Command/GenerateUuidCommand.php
+++ Command/GenerateUuidCommand.php
@@ -2,41 +2,16 @@
 
 namespace Dontdrinkandroot\DoctrineBundle\Command;
 
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
-use Dontdrinkandroot\DoctrineBundle\Event\Listener\UuidEntityListener;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GenerateUuidCommand extends Command
 {
-    /**
-     * @var UuidEntityListener
-     */
-    private $uuidEntityListener;
-
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
-
-    public function __construct(UuidEntityListener $uuidEntityListener, EntityManagerInterface $entityManager)
+    public function __construct()
     {
         parent::__construct();
-        $this->uuidEntityListener = $uuidEntityListener;
-        $this->entityManager = $entityManager;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function configure()
-    {
-        $this
-            ->addOption('strategy', null, InputOption::VALUE_REQUIRED);
     }
 
     /**
@@ -44,11 +19,6 @@ class GenerateUuidCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $strategy = $this->uuidEntityListener->getStrategy();
-        if ($input->getOption('strategy')) {
-            $strategy = $input->getOption('strategy');
-        }
-
-        $output->writeln($this->uuidEntityListener->generateUuid($this->entityManager, $strategy));
+        $output->writeln(Uuid::uuid4()->toString());
     }
 }
--- Command/SetUuidsCommand.php
+++ Command/SetUuidsCommand.php
@@ -3,22 +3,19 @@
 namespace Dontdrinkandroot\DoctrineBundle\Command;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
-use Dontdrinkandroot\DoctrineBundle\Event\Listener\UuidEntityListener;
 use Dontdrinkandroot\Entity\UuidEntityInterface;
+use Dontdrinkandroot\Event\Listener\UuidEntityListener;
 use Dontdrinkandroot\Repository\OrmEntityRepository;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SetUuidsCommand extends Command
 {
     /**
-     * @var UuidEntityListener
+     * @var \Dontdrinkandroot\Event\Listener\UuidEntityListener
      */
     private $uuidEntityListener;
 
@@ -27,10 +24,9 @@ class SetUuidsCommand extends Command
      */
     private $registry;
 
-    public function __construct(Registry $registry, UuidEntityListener $uuidEntityListener)
+    public function __construct(Registry $registry)
     {
         parent::__construct();
-        $this->uuidEntityListener = $uuidEntityListener;
         $this->registry = $registry;
     }
 
@@ -40,8 +36,7 @@ class SetUuidsCommand extends Command
     protected function configure()
     {
         $this
-            ->addArgument('entity', InputArgument::REQUIRED)
-            ->addOption('strategy', null, InputOption::VALUE_REQUIRED);
+            ->addArgument('entity', InputArgument::REQUIRED);
     }
 
     /**
@@ -49,17 +44,10 @@ class SetUuidsCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        /** @var UuidEntityListener $uuidListener */
-        $strategy = $this->uuidEntityListener->getStrategy();
-        if ($input->getOption('strategy')) {
-            $strategy = $input->getOption('strategy');
-        }
-
         $entityName = $input->getArgument('entity');
 
         $repository = $this->registry->getRepository($entityName);
         $entityManager  = $this->registry->getManagerForClass($entityName);
-
 
         $entities = $repository->findAll();
         foreach ($entities as $entity) {
@@ -67,10 +55,10 @@ class SetUuidsCommand extends Command
                 /** @var UuidEntityInterface $uuidEntity */
                 $uuidEntity = $entity;
                 if (null === $uuidEntity->getUuid()) {
-                    $uuidEntity->setUuid($uuidListener->generateUuid($entityManager, $strategy));
+                    $uuidEntity->setUuid(Uuid::uuid4()->toString());
                 }
-                $repository->flush($entity);
             }
         }
+        $entityManager->flush();
     }
 }
