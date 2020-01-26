c5b9be4027b71fb592c8a22f9e1019ca478bf807
Philip Washington Sorst <philip@sorst.net>
2020-01-26 22:16:23 +0100

    PHP Minimum 7.2, KernelEventListener: Only acting on master request

--- Event/Listener/TransactionalKernelEventListener.php
+++ Event/Listener/TransactionalKernelEventListener.php
@@ -5,74 +5,85 @@ namespace Dontdrinkandroot\DoctrineBundle\Event\Listener;
 use Dontdrinkandroot\Repository\TransactionManager;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
 
+/**
+ * Implements the Session in View Pattern.
+ *
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
 class TransactionalKernelEventListener
 {
-    /**
-     * @var LoggerInterface;
-     */
+    /** @var LoggerInterface; */
     private $logger;
 
-    /**
-     * @var TransactionManager
-     */
+    /** @var TransactionManager */
     private $transactionManager;
 
-    private $rollbackCodes = [];
+    private $rollbackCodes = [
+        Response::HTTP_BAD_REQUEST,
+        Response::HTTP_INTERNAL_SERVER_ERROR
+    ];
 
     public function __construct(TransactionManager $transactionManager)
     {
-        $this->transactionManager = $transactionManager;
         $this->logger = new NullLogger();
-        $this->rollbackCodes[] = Response::HTTP_BAD_REQUEST;
-        $this->rollbackCodes[] = Response::HTTP_INTERNAL_SERVER_ERROR;
+        $this->transactionManager = $transactionManager;
     }
 
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelRequest(RequestEvent $event)
     {
-        $this->getLogger()->info('Kernel Transaction: Begin');
-        $this->transactionManager->beginTransaction();
+        if ($event->isMasterRequest()) {
+            $this->getLogger()->info('Kernel Transaction: Begin');
+            $this->transactionManager->beginTransaction();
+        }
     }
 
     public function onKernelFinishRequest(FinishRequestEvent $event)
     {
-        $this->getLogger()->info('Kernel Transaction: Commit');
-        $this->transactionManager->commitTransaction();
+        if ($event->isMasterRequest()) {
+            $this->getLogger()->info('Kernel Transaction: Commit');
+            $this->transactionManager->commitTransaction();
+        }
     }
 
-    public function onKernelTerminate(PostResponseEvent $event)
+    public function onKernelTerminate(TerminateEvent $event)
     {
-        $response = $event->getResponse();
-        $statusCode = (int)$response->getStatusCode();
-        if (in_array($statusCode, $this->rollbackCodes)) {
-            $this->getLogger()->info('Kernel Transaction: Rollback by statusCode', ['statusCode' => $statusCode]);
+        if ($event->isMasterRequest()) {
+            $response = $event->getResponse();
+            $statusCode = $response->getStatusCode();
+            if (in_array($statusCode, $this->rollbackCodes)) {
+                $this->getLogger()->info('Kernel Transaction: Rollback by statusCode', ['statusCode' => $statusCode]);
+                $this->transactionManager->rollbackTransaction();
+            }
+        }
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        if ($event->isMasterRequest()) {
+            $this->getLogger()->info(
+                'Kernel Transaction: Rollback by exception',
+                [$event->getThrowable()->getMessage()]
+            );
             $this->transactionManager->rollbackTransaction();
         }
     }
 
-    public function onKernelException(GetResponseForExceptionEvent $event)
-    {
-        $this->getLogger()->info('Kernel Transaction: Rollback by exception', [$event->getException()->getMessage()]);
-        $this->transactionManager->rollbackTransaction();
-    }
-
-    /**
-     * @return LoggerInterface
-     */
-    public function getLogger()
+    public function getLogger(): LoggerInterface
     {
         return $this->logger;
     }
 
-    /**
-     * @param LoggerInterface $logger
-     */
-    public function setLogger($logger)
+    public function setLogger(LoggerInterface $logger)
     {
         $this->logger = $logger;
     }
