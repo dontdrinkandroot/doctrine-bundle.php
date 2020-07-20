7e9457b2a467350b8ad7c33b72a07c416d4151d1
Philip Washington Sorst <philip@sorst.net>
2020-07-20 10:59:22 +0200

    Adding TestKernel

---
+++ Tests/Integration/TestKernelTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Dontdrinkandroot\DoctrineBundle\Tests\Integration;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TestKernelTest extends WebTestCase
+{
+    public function testIsWorking()
+    {
+        self::bootKernel();
+    }
+}
---
+++ Tests/TestApp/TestKernel.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Dontdrinkandroot\DoctrineBundle\Tests\TestApp;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+class TestKernel extends Kernel
+{
+    use MicroKernelTrait;
+
+    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        $contents = require $this->getProjectDir() . '/Tests/TestApp/config/bundles.php';
+        foreach ($contents as $class => $envs) {
+            if (isset($envs['all']) || isset($envs[$this->environment])) {
+                yield new $class();
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $confDir = $this->getProjectDir() . '/Tests/TestApp/config';
+
+        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $container->addResource(new FileResource($this->getProjectDir() . '/Tests/TestApp/config/bundles.php'));
+        // Feel free to remove the "container.autowiring.strict_mode" parameter
+        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
+        $container->setParameter('container.autowiring.strict_mode', true);
+        $container->setParameter('container.dumper.inline_class_loader', true);
+        $confDir = $this->getProjectDir() . '/Tests/TestApp/config';
+
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir() . '/ddr_doctrine_bundle/cache/';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLogDir()
+    {
+        return sys_get_temp_dir() . '/ddr_doctrine_bundle/logs/';
+    }
+}
