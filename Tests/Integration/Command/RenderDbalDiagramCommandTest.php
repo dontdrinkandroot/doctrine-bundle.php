99171119d9077d58ad9fa6df0898d5722e9aa7ee
Philip Washington Sorst <philip@sorst.net>
2021-12-15 11:58:54 +0100

    Silencing legacy warnings

--- Tests/Integration/Command/RenderDbalDiagramCommandTest.php
+++ Tests/Integration/Command/RenderDbalDiagramCommandTest.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
+/**
+ * @group legacy
+ */
 class RenderDbalDiagramCommandTest extends KernelTestCase
 {
     public function testExecute(): void
--- Tests/Integration/Command/RenderOrmDiagramCommandTest.php
+++ Tests/Integration/Command/RenderOrmDiagramCommandTest.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 
+/**
+ * @group legacy
+ */
 class RenderOrmDiagramCommandTest extends KernelTestCase
 {
     public function testExecute(): void
